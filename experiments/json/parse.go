0228c104bedb5a0309de093ada0e784dae07d2e8
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-04-07 11:42:33 +0200

    we can first read the key and then stringified value - we don't need to unmarshal it

--- experiments/json/parse.go
+++ experiments/json/parse.go
@@ -15,13 +15,23 @@ func main() {
 }
 
 func run() error {
-	input := strings.NewReader(`[{"foo": 12, "bar": [1,2], "nested": {"value": 12}}]`)
+	raw := `[{"foo": 12, "bar": [1,2], "nested": {"value": 12}}]`
+	fmt.Printf("got input: %v\n", raw)
+	input := strings.NewReader(raw)
 
 	dec := json.NewDecoder(input)
+	var next *json.RawMessage
 	for {
 		tok, err := dec.Token()
 		if err == io.EOF {
 			break
+		}
+		if _, ok := tok.(string); ok {
+			fmt.Printf("\tgot a key - looking for a value! %v\n", tok)
+			if err := dec.Decode(&next); err != nil {
+				return err
+			}
+			fmt.Printf("\tgot next: %s\n", *next)
 		}
 		if err != nil {
 			return err
