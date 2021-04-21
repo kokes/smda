3880f93b5ce52c9c9a2e9c777c9caf0f36bbeed6
Ondrej Kokes <ondrej.kokes@gmail.com>
2021-04-21 16:43:44 +0200

    cleaning up expressions

---
+++ src/query/expr/expression_test.go
@@ -0,0 +1,81 @@
+package expr
+
+import (
+	"reflect"
+	"testing"
+)
+
+func stringifySlice(exprs []*Expression) []string {
+	if len(exprs) == 0 {
+		return nil
+	}
+	ret := make([]string, 0, len(exprs))
+	for _, expr := range exprs {
+		ret = append(ret, expr.String())
+	}
+	return ret
+}
+
+func TestAggExpr(t *testing.T) {
+	tests := []struct {
+		raw      string
+		expected []string
+		err      error
+	}{
+		{"1", nil, nil},
+		{"1 + nullif(foo) - bar", nil, nil},
+		{"min(a)", []string{"min(a)"}, nil},
+		{"min(a) + min(b)", []string{"min(a)", "min(b)"}, nil},
+		{"4*min(a) + 3-min(b)", []string{"min(a)", "min(b)"}, nil},
+		{"2*nullif(min(a) + 3*min(b))", []string{"min(a)", "min(b)"}, nil},
+		{"min(a)*min(b)*min(c)", []string{"min(a)", "min(b)", "min(c)"}, nil},
+		// nested aggexprs
+		{"min(5*min(a))", nil, errNoNestedAggregations},
+		{"sum(max(b))", nil, errNoNestedAggregations},
+		{"1-sum(nullif(foo, max(bar)))", nil, errNoNestedAggregations},
+	}
+	for _, test := range tests {
+		expr, err := ParseStringExpr(test.raw)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		res, err := AggExpr(expr)
+		if err != test.err {
+			t.Errorf("expecting %s to result in error %+v, got %+v instead", test.raw, test.err, err)
+		}
+		ress := stringifySlice(res)
+		if !reflect.DeepEqual(ress, test.expected) {
+			t.Errorf("expected %+v to have %+v as aggregating expressions, got %+v instead", test.raw, test.expected, ress)
+		}
+	}
+}
+
+func TestExprStringer(t *testing.T) {
+	tests := []struct {
+		raw      string
+		expected string
+	}{
+		{"1+2+ 3", "1+2+3"},
+		{"1+(2+ 3)", "1+(2+3)"},
+		{"max( foo) - 3", "max(foo)-3"},
+		{"2 * (foo-BAR)", "2*(foo-bar)"},
+		{"(foo-BAR)*2", "(foo-bar)*2"},
+		{"(foo-(3-BAR))*2", "(foo-(3-bar))*2"},
+		{"foo = 'bar'", "foo='bar'"},
+	}
+
+	for _, test := range tests {
+		parsed, err := ParseStringExpr(test.raw)
+		if err != nil {
+			t.Fatalf("expression %+v failed: %v", test.raw, err)
+			continue
+		}
+		if parsed.String() != test.expected {
+			t.Errorf("expecting %s to parse and then stringify into %s, got %s instead", test.raw, test.expected, parsed.String())
+		}
+	}
+}
+
+// func (expr *Expression) UnmarshalJSON(data []byte) error {
+// MarshalJSON
--- src/query/expr/parser.go
+++ src/query/expr/parser.go
@@ -1,212 +1,16 @@
 package expr
 
-import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"strings"
-
-	"github.com/kokes/smda/src/column"
-)
-
-var errNoNestedAggregations = errors.New("cannot nest aggregations (e.g. sum(min(a)))")
-
-type exprType uint8
-
+// thank you, Thorsten
 const (
-	exprInvalid exprType = iota
-	exprIdentifier
-	exprIdentifierQuoted
-	exprAnd
-	exprOr
-	exprAddition
-	exprSubtraction
-	exprMultiplication
-	exprDivision
-	exprEquality
-	exprNequality
-	exprLessThan
-	exprLessThanEqual
-	exprGreaterThan
-	exprGreaterThanEqual
-	exprLiteralInt
-	exprLiteralFloat
-	exprLiteralBool
-	exprLiteralString
-	exprLiteralNull
-	exprFunCall
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
 )
-
-func (expr *Expression) IsIdentifier() bool {
-	return expr.etype == exprIdentifier || expr.etype == exprIdentifierQuoted
-}
-func (expr *Expression) IsOperatorBoolean() bool {
-	return expr.etype == exprAnd || expr.etype == exprOr
-}
-func (expr *Expression) IsOperatorMath() bool {
-	return expr.etype >= exprAddition && expr.etype <= exprDivision
-}
-func (expr *Expression) IsOperatorComparison() bool {
-	return expr.etype >= exprEquality && expr.etype <= exprGreaterThanEqual
-}
-
-func (expr *Expression) IsOperator() bool {
-	return expr.IsOperatorBoolean() || expr.IsOperatorMath() || expr.IsOperatorComparison()
-}
-func (expr *Expression) IsLiteral() bool {
-	return expr.etype >= exprLiteralInt && expr.etype <= exprLiteralNull
-}
-
-// ARCH: is this used anywhere? (partially in the Expression stringer, partially in error reporting in parser_test.go)
-func (etype exprType) String() string {
-	switch etype {
-	case exprInvalid:
-		return "Invalid"
-	case exprIdentifier:
-		return "Identifier"
-	case exprIdentifierQuoted:
-		return "QuotedIdentifier"
-	case exprAnd:
-		return "AND"
-	case exprOr:
-		return "OR"
-	case exprAddition:
-		return "+"
-	case exprSubtraction:
-		return "-"
-	case exprMultiplication:
-		return "*"
-	case exprDivision:
-		return "/"
-	case exprEquality:
-		return "="
-	case exprNequality:
-		return "!="
-	case exprLessThan:
-		return "<"
-	case exprLessThanEqual:
-		return "<="
-	case exprGreaterThan:
-		return ">"
-	case exprGreaterThanEqual:
-		return ">="
-	case exprLiteralInt:
-		return "LiteralInt"
-	case exprLiteralFloat:
-		return "LiteralFloat"
-	case exprLiteralBool:
-		return "LiteralBool"
-	case exprLiteralString:
-		return "LiteralString"
-	case exprLiteralNull:
-		return "LiteralNull"
-	case exprFunCall:
-		return "FunCall"
-	default:
-		return "unknown_expression"
-	}
-}
-
-type Expression struct {
-	etype             exprType
-	children          []*Expression
-	value             string
-	parens            bool
-	evaler            func(...column.Chunk) (column.Chunk, error)
-	aggregator        *column.AggState
-	aggregatorFactory func(...column.Dtype) (*column.AggState, error)
-}
-
-func (expr *Expression) InitAggregator(schema column.TableSchema) error {
-	var rtypes []column.Dtype
-	for _, ch := range expr.children {
-		rtype, err := ch.ReturnType(schema)
-		if err != nil {
-			return err
-		}
-		rtypes = append(rtypes, rtype.Dtype)
-	}
-	aggregator, err := expr.aggregatorFactory(rtypes...)
-	if err != nil {
-		return err
-	}
-	expr.aggregator = aggregator
-	return nil
-}
-
-func AggExpr(expr *Expression) ([]*Expression, error) {
-	var ret []*Expression
-	found := false
-	if expr.etype == exprFunCall && expr.evaler == nil {
-		ret = append(ret, expr)
-		found = true
-	}
-	for _, ch := range expr.children {
-		ach, err := AggExpr(ch)
-		if err != nil {
-			return nil, err
-		}
-		if ach != nil {
-			if found {
-				return nil, errNoNestedAggregations
-			}
-			ret = append(ret, ach...)
-		}
-	}
-	return ret, nil
-}
-
-func (expr *Expression) String() string {
-	var rval string
-	switch expr.etype {
-	case exprInvalid:
-		rval = "invalid_expression"
-	case exprIdentifier:
-		rval = expr.value
-	case exprIdentifierQuoted:
-		rval = fmt.Sprintf("\"%s\"", expr.value)
-	case exprAddition, exprSubtraction, exprMultiplication, exprDivision, exprEquality,
-		exprNequality, exprLessThan, exprLessThanEqual, exprGreaterThan, exprGreaterThanEqual, exprAnd, exprOr:
-		rval = fmt.Sprintf("%s %s %s", expr.children[0], expr.etype, expr.children[1])
-	case exprLiteralInt, exprLiteralFloat, exprLiteralBool:
-		rval = expr.value
-	case exprLiteralString:
-		// TODO: what about literals with apostrophes in them? escape them
-		rval = fmt.Sprintf("'%s'", expr.value)
-	case exprLiteralNull:
-		rval = "NULL"
-	case exprFunCall:
-		args := make([]string, 0, len(expr.children))
-		for _, ch := range expr.children {
-			args = append(args, ch.String())
-		}
-
-		rval = fmt.Sprintf("%s(%s)", expr.value, strings.Join(args, ", "))
-	default:
-		// we need to panic, because we use this stringer for expression comparison
-		panic(fmt.Sprintf("unsupported expression type: %v", expr.etype))
-	}
-	if expr.parens {
-		return fmt.Sprintf("(%s)", rval)
-	}
-	return rval
-}
-
-func (expr *Expression) UnmarshalJSON(data []byte) error {
-	var raw string
-	if err := json.Unmarshal(data, &raw); err != nil {
-		return err
-	}
-	ex, err := ParseStringExpr(raw)
-	if ex != nil {
-		*expr = *ex
-	}
-	return err
-}
-
-func (expr *Expression) MarshalJSON() ([]byte, error) {
-	return json.Marshal(expr.String())
-}
 
 // limitations (fix this for the custom_parser - TODO(PR)):
 // - cannot use this for full query parsing, just expressions
@@ -228,8 +32,29 @@ func ParseStringExpr(s string) (*Expression, error) {
 		return nil, err
 	}
 	// TODO(PR)
-	_ = tokens
-	tree := &Expression{}
+	pos := 0
+	for pos < len(tokens) {
 
-	return tree, nil
+		pos++ // just so it doesn't explode
+	}
+	// func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	// 	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	// 	stmt.Expression = p.parseExpression(LOWEST)
+	// 	if p.peekTokenIs(token.SEMICOLON) {
+	// 		p.nextToken()
+	// 	}
+	// 	return stmt
+	// }
+
+	return &Expression{}, nil
 }
+
+// func (p *Parser) parseExpression(precedence int) ast.Expression {
+// 	prefix := p.prefixParseFns[p.curToken.Type]
+// 	if prefix == nil {
+// 		return nil
+// 	}
+// 	leftExp := prefix()
+// 	return leftExp
+// }
