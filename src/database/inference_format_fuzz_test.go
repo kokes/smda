f30f277641390effb3be46955465995bc34ea790
Ondrej Kokes <ondrej.kokes@gmail.com>
2022-03-23 16:49:52 +0100

    resolve todos

--- src/database/inference_format_fuzz_test.go
+++ src/database/inference_format_fuzz_test.go
@@ -5,33 +5,22 @@ import (
 )
 
 func FuzzDelimiterInference(f *testing.F) {
-	f.Add(123)
-	f.Fuzz(func(t *testing.T, raw int) {
-		delim := inferDelimiter([]byte("..."))
+	f.Add([]byte("foo,bar,baz\n1,2,3\n4,5,6\n"))
+	f.Fuzz(func(t *testing.T, payload []byte) {
+		delim := inferDelimiter(payload)
 		if delim == delimiterNone {
-			t.Skip() // TODO(PR): are we doing this right?
+			t.Skip()
 		}
 		t.Skip()
 	})
 }
-
-// func FuzzDelimiterInference(f *testing.F) {
-// 	f.Add([]byte("foo,bar,baz\n1,2,3\n4,5,6\n"))
-// 	f.Fuzz(func(t *testing.T, payload []byte) {
-// 		delim := inferDelimiter(payload)
-// 		if delim == delimiterNone {
-// 			t.Skip() // TODO(PR): are we doing this right?
-// 		}
-// 		t.Skip()
-// 	})
-// }
 
 func FuzzCompressionInference(f *testing.F) {
 	f.Add([]byte("abc"))
 	f.Fuzz(func(t *testing.T, payload []byte) {
 		cmp := inferCompression(payload)
 		if cmp == compressionNone {
-			t.Skip() // TODO(PR): are we doing this right?
+			t.Skip()
 		}
 	})
 }
